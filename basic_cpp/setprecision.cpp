030c255d4553f79279def03e5994091a20ff7e64
aapdo <jade04342@gmail.com>
2024-07-16 18:56:16 +0900

    gitignore

---
+++ basic_cpp/setprecision.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    double a = 1.23456789;
+    typedef long long ll;
+    cout << a << "\n";
+    cout << fixed << setprecision(3) << a << "\n";
+    cout << fixed << setprecision(8) << a << "\n";
+}
