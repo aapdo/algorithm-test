eacee6a1947c27d724769e1826eea2950d4d888c
aapdo <jade04342@gmail.com>
2024-07-21 16:25:11 +0900

    boj 11655

--- baekjoon/1/1_F_11655.cpp
+++ baekjoon/1/1_F_11655.cpp
@@ -5,5 +5,13 @@ string s;
 
 int main(){
     getline(cin, s);
+    for(int i = 0; i < s.length(); i++){
+        if('A' <= s[i] && s[i] <= 'Z'){
+            cout << (char) (( ( (s[i] - 'A') + 13 ) % 26 ) + 'A');
+        }else if('a' <= s[i] && s[i] <= 'z'){
+            cout << (char) (( ( (s[i] - 'a') + 13 ) % 26 ) + 'a');
+        }
+        else cout << s[i];
+    }
     
 }
